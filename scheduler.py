af93c1b61d3da3855fd625375e5ef83b97aa8af3
Michael Korzon <mclzor@gmail.com>
2016-04-06 12:34:05 -0500

    Added files via upload

--- scheduler.py
+++ scheduler.py
@@ -1,18 +1,47 @@
-#!venv/bin/python
-
+#!/venv/bin/python
 from recordclass import recordclass
 from datetime import datetime, timedelta
 from csv import reader
 from datetime import datetime
+from itertools import combinations
 
 
-Task = recordclass('Task', 'name deadline duration min_dur max_dur max_dur_daily recurrence')
-Event = recordclass('Event', 'name start duration')
-Recurrence = recordclass('Recurrence', 'type occurrences')
+class Task:
+    def __init__(self, name=None, deadline=None, duration=None, min_dur=None,
+                 max_dur=None, max_dur_daily=None, recurrence=None):
+        self.name = name
+        self.deadline = deadline
+        self.duration = duration
+        self.min_dur = min_dur
+        self.max_dur = max_dur
+        self.max_dur_daily = max_dur_daily
+        self.recurrence = recurrence
 
-t_step = timedelta(minutes=15)
+    def __hash__(self):
+        return hash(self.name)
+
+
+class Event:
+    def __init__(self, name=None, start=None, duration=None):
+        self.name = name
+        self.start = start
+        self.duration = duration
+
+    def __hash__(self):
+        return hash(self.name)
+
+
+class Recurrence:
+    def __init__(self, type=None, occurrences=None):
+        self.type = type
+        self.occurrences = occurrences
+
+    def __hash__(self):
+        return hash(self.occurrences)
+
 
 tformat = '%Y/%m/%d%H:%M'
+
 
 def read_data(filename):
     tasks = list()
@@ -26,6 +55,7 @@ def read_data(filename):
                 events.append(parse_event(line))
 
     return (tasks, events)
+
 
 def parse_task(line):
     return Task(name=line[1],
@@ -42,7 +72,7 @@ def parse_event(line):
                  start=datetime.strptime(line[2], tformat),
                  duration=strf_timedelta(line[3]))
 
-                           
+
 def strf_timedelta(s):
     try:
         return timedelta(hours=int(s[:2]), minutes=int(s[3:]))
@@ -54,7 +84,6 @@ def strf_datetime(s):
     if len(s) > 0:
         datetime.strptime(s, tformat)
 
-    
 
 def parse_recurrence(s):
     try:
@@ -68,7 +97,7 @@ def split_tasks(tasks):
     for task in tasks:
         events += split_task(task)
     return events
-        
+
 
 def split_task(task):
     rem = task.duration
@@ -78,15 +107,15 @@ def split_task(task):
         rem -= duration
         if duration == rem:
             rem == timedelta()
-        
+
         events.append(Event(name=task.name,
                             start=None,
                             duration=duration))
-                      
+
     return events
 
 
-def is_consistent(tasks, events, constraints):
+def is_consistent(events, constraints):
     for constraint in constraints:
         if not constraint(events):
             return False
@@ -100,9 +129,12 @@ def is_complete(events):
     return True
 
 
+# Constraints
 def durations_met(events, tasks):
     for task in tasks:
-        if not duration_met(task, filter(lambda e: e.name == task.name, events)):
+        if not duration_met(
+                task,
+                filter(lambda e: e.name == task.name, events)):
             return False
     return True
 
@@ -112,22 +144,84 @@ def duration_met(task, events):
     for event in events:
         duration += event.duration
     return duration >= task.duration
-        
+
 
 def deadline_met(events, tasks):
     for task in tasks:
-        if not duration_met(task, filter(lambda e: e.name == task.name and
-                                         e.start+e.duration < task.deadline, events)):
+        if not duration_met(task,
+                            filter(lambda e: e.start and task.deadline and
+                                   e.name == task.name and
+                                   e.start+e.duration < task.deadline,
+                                   events)):
             return False
     return True
 
+
 def non_overlapping(events):
-    for i, event in enumerate(events):
-        for j in range(i, len(events)):
-            if not event == events[i]:
-                if event.start < events[i].start:
-                    if event.start+event.duration > events[i].start:
-                        return 
+    for event1, event2 in combinations(events, 2):
+        if overlaps(event1, event2):
+            return False
+    return True
+
+
+def overlaps(event1, event2):
+    if not event1.start or not event2.start:
+        return False
+    if event1.start < event2.start:
+        return event1.start + event1.duration > event2.start
+    elif event1.start > event2.start:
+        return event2.start + event2.duration > event1.start
+    else:
+        return False
+
+
+# Solution is a dict
+def csp_solve(solution, domain, constraints):
+    if is_consistent(solution, constraints) and is_complete(solution):
+        return solution
+
+    temp_solution = solution
+    for event in solution:
+        print(event.start)
+        if not event.start:
+            for candidate in domain[event]:
+                event.start = candidate
+                if csp_solve(solution, domain, constraints):
+                    return solution
+                else:
+                    csp_solve(temp_solution, domain, constraints)
+    return list()
+
+
+def init_domain(events):
+    domain = dict()
+    now = datetime.now()
+    for event in events:
+        if event.start:
+            domain[event] = [event.start]
+        else:
+            domain[event] = all_time_slots(now, now + timedelta(days=7))
+    return domain
+
+
+def all_time_slots(start, end):
+    slots = list()
+    while start < end:
+        slots.append(start)
+        start += timedelta(minutes=15)
+    return slots
+
+
 if __name__ == '__main__':
     tasks, events = read_data('schedule.txt')
-    print(split_tasks(tasks))
+    events += split_tasks(tasks)
+    domain = init_domain(events)
+
+    constraints = {
+        non_overlapping,
+        lambda e: durations_met(e, tasks),
+        lambda e: deadline_met(e, tasks),
+    }
+    
+    solution = csp_solve(events, domain, constraints)
+    print(solution)
